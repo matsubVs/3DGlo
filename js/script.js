987d229c7dfffbd9db2c453565422e0585cf072c
Danila Sokolov <matsubus@mail.ru>
2020-12-15 20:30:45 +0300

    feat(lesson18): completed lesson18

---
+++ js/script.js
@@ -0,0 +1,55 @@
+window.addEventListener("DOMContentLoaded", () => {
+    "use sctric";
+
+    //Timer
+    function countTimer(deadline) {
+        const timerHours = document.querySelector('#timer-hours'),
+            timerMinutes = document.querySelector('#timer-minutes'),
+            timerSeconds = document.querySelector('#timer-seconds');
+
+        function getTimeRemaining() {
+            const dateStop = new Date(deadline).getTime(),
+                dateNow = new Date().getTime(),
+                timeRemaining = (dateStop - dateNow) / 1000,
+                seconds = Math.floor(timeRemaining % 60),
+                minutes = Math.floor((timeRemaining / 60) % 60),
+                hours = Math.floor(timeRemaining / 60 / 60);
+
+            return {
+                timeRemaining,
+                hours,
+                minutes,
+                seconds
+            };
+        }
+
+        function checkTime(time) {
+            time = String(time);
+            if (time.length < 2) {
+                return "0" + time;
+            } else {
+                return time;
+            }
+        }
+
+        let intervalId;
+        function updateTime() {
+            const timer = getTimeRemaining();
+
+            timerHours.textContent = checkTime(timer.hours);
+            timerMinutes.textContent = checkTime(timer.minutes);
+            timerSeconds.textContent = checkTime(timer.seconds);
+
+            if (timer.timeRemaining < 0) {
+                clearInterval(intervalId);
+                timerHours.textContent = "00";
+                timerMinutes.textContent = "00";
+                timerSeconds.textContent = "00";
+            }
+        }
+
+        intervalId = setInterval(updateTime, 1000);
+    }
+
+    countTimer('16 december 2020');
+});
