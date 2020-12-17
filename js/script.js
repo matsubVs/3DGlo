4d566f20e9d973fa84e24e5100b23afe3c85f492
Danila Sokolov <matsubus@mail.ru>
2020-12-18 01:11:19 +0300

    feat(lesson21): slider relesase

--- js/script.js
+++ js/script.js
@@ -73,6 +73,25 @@ window.addEventListener("DOMContentLoaded", () => {
 
     countTimer('18 december 2020');
 
+    // Create Dots
+    const dotParent = document.querySelector('.portfolio-dots'),
+        slideCount = document.querySelectorAll('.portfolio-item').length;
+
+
+    const createDots = (count, parent) => {
+        for (let i = 0; i < count; i++) {
+            const li = document.createElement('li');
+            li.classList.add('dot');
+            if (i === 0) {
+                li.classList.add('dot-active');
+            }
+
+            parent.appendChild(li);
+        }
+    };
+
+    createDots(slideCount, dotParent);
+
     // Menu
     const toggleMenu = () => {
 
@@ -89,7 +108,6 @@ window.addEventListener("DOMContentLoaded", () => {
 
         body.addEventListener('click', event => {
             let target = event.target;
-            console.log(target);
             if (target.closest(".menu")) {
                 menuHandler();
             } else if (target === closeBtn) {
@@ -181,4 +199,94 @@ window.addEventListener("DOMContentLoaded", () => {
     };
 
     togglePopup();
+
+    //slider
+    const slider = () => {
+        const slide = document.querySelectorAll('.portfolio-item'),
+            dot = document.querySelectorAll('.dot'),
+            slider = document.querySelector('.portfolio-content');
+
+        let currentSlide = 0,
+            interval;
+
+        const prevSlide = (elem, index, srtClass) => {
+            elem[index].classList.remove(srtClass);
+        };
+
+        const nextSlide = (elem, index, srtClass) => {
+            elem[index].classList.add(srtClass);
+        };
+
+        const autoPlaySlide = () => {
+            prevSlide(slide, currentSlide, 'portfolio-item-active');
+            prevSlide(dot, currentSlide, 'dot-active');
+            currentSlide++;
+            if (currentSlide >= slide.length) {
+                currentSlide = 0;
+            }
+            nextSlide(slide, currentSlide, 'portfolio-item-active');
+            nextSlide(dot, currentSlide, 'dot-active');
+        };
+
+        const startSlide = (time = 3000) => {
+            interval = setInterval(autoPlaySlide, time);
+        };
+
+        const stopSlide = () => {
+            clearInterval(interval);
+        };
+
+        slider.addEventListener('click', event => {
+            event.preventDefault();
+
+            // eslint-disable-next-line prefer-const
+            let target = event.target;
+
+            if (!target.matches('.portfolio-btn, .dot')) {
+                return;
+            }
+
+            prevSlide(slide, currentSlide, 'portfolio-item-active');
+            prevSlide(dot, currentSlide, 'dot-active');
+
+            if (target.matches('#arrow-right')) {
+                currentSlide++;
+            } else if (target.matches('#arrow-left')) {
+                currentSlide--;
+            } else if (target.matches('.dot')) {
+                dot.forEach((item, index) => {
+                    if (item === target) {
+                        currentSlide = index;
+                    }
+                });
+            }
+
+            if (currentSlide >= slide.length) {
+                currentSlide = 0;
+            } else if (currentSlide < 0) {
+                currentSlide = slide.length - 1;
+            }
+
+            nextSlide(slide, currentSlide, 'portfolio-item-active');
+            nextSlide(dot, currentSlide, 'dot-active');
+        });
+
+        slider.addEventListener('mouseover', event => {
+            if (event.target.matches('.portfolio-btn') || event.target.matches('.dot')) {
+                stopSlide();
+            }
+        });
+
+        
+        slider.addEventListener('mouseout', event => {
+            if (event.target.matches('.portfolio-btn') || event.target.matches('.dot')) {
+                startSlide();
+            }
+        });
+
+        startSlide();
+    };
+
+    slider();
+    
 });
