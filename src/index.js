6c536501478c3eb6338d5325f6463ce1623ef4c4
Danila Sokolov <matsubus@mail.ru>
2021-01-10 17:15:11 +0300

    feat(lesson32): completed lesson32

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import createDots from './modules/createDots';
 import toggleMenu from './modules/toggleMenu';
 import togglePopup from './modules/togglePopup';
 import tabs from './modules/tabs';
-import slider from './modules/slider';
+// import slider from './modules/slider';
 import changePhoto from './modules/changePhoto';
 import inputValidation from './modules/inputValidation';
 import calc from './modules/calc';
@@ -27,7 +27,7 @@ togglePopup();
 tabs();
 
 //slider
-slider();
+// slider();
 
 // change photo
 changePhoto();
